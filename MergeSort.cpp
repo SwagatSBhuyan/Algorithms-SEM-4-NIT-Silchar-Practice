f6bad334b9a3384a799cabfa7188580d55028793
Swagat S. Bhuyan <41104465+SwagatSBhuyan@users.noreply.github.com>
2020-08-05 15:21:15 +0530

    Commit2_MERGE_SORT

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -3,15 +3,45 @@
 using namespace std;
 
 
-void InsertionSort(int* Arr, int size)
+void Merge(int* Arr, int p, int q, int r)
 {
-    for (int j = 1; j < size; j++)
+    int i = 0, j = 0, k = 0;
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    int L[1000], R[1000];
+    L[n1] = INT_MAX;
+    R[n2] = INT_MAX;
+
+    for (i = 0; i < n1; i++)
+        L[i] = Arr[p + i];
+    for (j = 0; j < n2; j++)
+        R[j] = Arr[q + j + 1];
+
+    i = 0, j = 0, k = p;
+    while (i < n1  && j < n2)
     {
-        int key = Arr[j];
-        int i = j - 1;
-        while (i >= 0 && Arr[i] > key)
-            Arr[i+1] = Arr[i--];
-        Arr[i+1] = key;
+        if (L[i] <= R[j])
+            Arr[k] = L[i++];
+        else
+            Arr[k] = R[j++];  
+        k++;         
+    }    
+    while (i < n1) 
+        Arr[k++] = L[i++]; 
+    while (j < n2) 
+        Arr[k++] = R[j++]; 
+
+}
+
+
+void MergeSort(int* Arr, int p, int r)
+{
+    if (p < r)
+    {
+        int q = floor((p + r)/2);
+        MergeSort(Arr, p, q);
+        MergeSort(Arr, q+1, r);
+        Merge(Arr, p, q, r);
     }
 }
 
@@ -45,7 +75,7 @@ system("CLS");
     cout << "\nOriginal Array: \n";
     PrintArray(A, size);
 
-    InsertionSort(A, size);
+    MergeSort(A, 0, size - 1);
 
     cout << "\nSorted Array: \n";
     PrintArray(A, size);
